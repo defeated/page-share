4a83cb0f2a51d1828d9c83d64f94c33d56990cbf
eddie cianci <defeated2k4@gmail.com>
2015-01-25 15:47:10 -0500

    use modular sinatra app since most settings are disabled by default

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'json'
 require 'base64'
 require './lib/cacher'
@@ -6,37 +6,34 @@ require './lib/fetcher'
 require './lib/extractor'
 require './lib/etagger'
 
-configure do
-  enable :logging
-  disable :run
-  disable :sessions
-  disable :method_override
-  disable :static
+class PageShareApp < Sinatra::Base
+  configure do
+    enable :logging
+    Fetcher.cache = Cacher.new
+  end
 
-  Fetcher.cache = Cacher.new
-end
+  get '/:encoded_url' do
+    url = Base64.decode64 params[:encoded_url]
+    result = Fetcher.new(url).fetch!
 
-get '/:encoded_url' do
-  url = Base64.decode64 params[:encoded_url]
-  result = Fetcher.new(url).fetch!
+    if result.success?
+      details = Extractor.new(result.content).extract!
+      type    = :json
+      payload = details.to_h.to_json
+      key     = "#{ url }:#{ payload }"
 
-  if result.success?
-    details = Extractor.new(result.content).extract!
-    type    = :json
-    payload = details.to_h.to_json
-    key     = "#{ url }:#{ payload }"
+      if callback = params[:callback]
+        type = :js
+        payload = "#{ callback }( #{ payload } );"
+      end
 
-    if callback = params[:callback]
-      type = :js
-      payload = "#{ callback }( #{ payload } );"
+      content_type type
+      expires 5.minutes, :public, :must_revalidate
+      etag Etagger.new(key).hash
+      payload
+    else
+      status 500
+      result.content
     end
-
-    content_type type
-    expires 5.minutes, :public, :must_revalidate
-    etag Etagger.new(key).hash
-    payload
-  else
-    status 500
-    result.content
   end
 end
