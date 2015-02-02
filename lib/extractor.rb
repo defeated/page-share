2c8c4e6367dd36e659826d705a1f022ec3808f66
eddie cianci <defeated2k4@gmail.com>
2015-02-02 00:07:25 -0500

    refactor Extractor based on reek feedback

--- lib/extractor.rb
+++ lib/extractor.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 
+# attempts to extract meaningful metadata from an html document
 class Extractor
   attr_reader :content
 
@@ -7,38 +8,73 @@ class Extractor
     @content = content
   end
 
-  def extract!
-    document = Nokogiri::HTML content do |config|
-      config.noerror.nonet
-    end
-
-    title       = document.at('meta[@property="og:title"]/@content') ||
-                  document.at('meta[@name="twitter:title"]/@content') ||
-                  document.at('title/text()')
-
-    description = document.at('meta[@property="og:description"]/@content') ||
-                  document.at('meta[@name="twitter:description"]/@content') ||
-                  document.at('meta[@name="description"]/@content')
-
-    canonical   = document.at('link[@rel="canonical"]/@href') ||
-                  document.at('meta[@property="og:url"]/@content')
-
-    image       = document.at('meta[@property="og:image"]/@content') ||
-                  document.at('meta[@name="twitter:image:src"]/@content')
-
-    favicon     = document.at('link[@rel="shortcut icon"]/@href') ||
-                  document.at('link[@rel="icon"]/@href')
-
-    Page.new(
-      title.to_s,
-      description.to_s,
-      canonical.to_s,
-      image.to_s,
-      favicon.to_s
-    )
+  def extract
+    parsed = Nokogiri::HTML content { |cfg| cfg.noerror.nonet }
+    Document.new parsed
   end
 
   private
 
-  Page = Struct.new(:title, :description, :canonical, :image, :favicon)
+  # contains all attribute search heuristics for an html document
+  class Document
+    def initialize(document)
+      @document = document
+    end
+
+    def to_h
+      {
+        title:        title,
+        description:  description,
+        canonical:    canonical,
+        image:        image,
+        favicon:      favicon
+      }
+    end
+
+    def title
+      @title ||= find_first(
+        'meta[@property="og:title"]/@content',
+        'meta[@name="twitter:title"]/@content',
+        'title/text()'
+      )
+    end
+
+    def description
+      @description ||= find_first(
+        'meta[@property="og:description"]/@content',
+        'meta[@name="twitter:description"]/@content',
+        'meta[@name="description"]/@content'
+      )
+    end
+
+    def canonical
+      @canonical ||= find_first(
+        'link[@rel="canonical"]/@href',
+        'meta[@property="og:url"]/@content'
+      )
+    end
+
+    def image
+      @image ||= find_first(
+        'meta[@property="og:image"]/@content',
+        'meta[@name="twitter:image:src"]/@content'
+      )
+    end
+
+    def favicon
+      @favicon ||= find_first(
+        'link[@rel="shortcut icon"]/@href',
+        'link[@rel="icon"]/@href'
+      )
+    end
+
+    private
+
+    def find_first(*xpaths)
+      xpaths.inject(nil) do |result, xpath|
+        result = @document.at xpath
+        break result if result
+      end.to_s
+    end
+  end
 end
